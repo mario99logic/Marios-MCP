dd40d5e5c6238dae7ee5f1c72c74e8538db55a01
Mario Rohana <mario@autokitteh.com>
2026-01-14 21:45:06 +0200

    feat: add tool name

---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create MCP server.
+mcp = FastMCP("Marios_MCP", json_response=True)
+
+
+@mcp.tool()
+def add(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello {name}"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
